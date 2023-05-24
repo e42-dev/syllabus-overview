28afda0bf488d0facbf916abf708de1b7102e1a0
Sesha Sai <nudurupatiseshasai14@gmail.com>
2023-05-24 15:34:00 +0530

    Last Commit

--- index.js
+++ index.js
@@ -1,68 +1,67 @@
 $(document).ready(function() {
-    $("#2").click(function() {
-      $(".cont").html("<h4>Elementary Data Structures and Algorithms</h4><p><b>About:</b>This module covers fundamentals of DSA. You'll gain problem-solving skills through practical exercises</p><p><b>Topics Covered:</b>ArrayList, Switch cases, Strings, Sorting, StringBuilder, BufferReader, Recursion & Data Interpretation</p>");
-  
-      // Add the CSS class
-      $(".cont").addClass("transi");
-  
-      // Remove the CSS class after a certain duration
-      setTimeout(function() {
-        $(".cont").removeClass("transi");
-      }, 1000);
-  
-      // Call the change_color function
-      change_color(12);
-    });
-  
-    $("#1").click(function() {
-      $(".cont").html("<h4>Data Structures and Algorithms</h4><p><b>About:</b>This module covers fundamentals of DSA. You'll gain problem-solving skills through practical exercises</p><p><b>Topics Covered:</b>ArrayList, Switch cases, Strings, Sorting, StringBuilder, BufferReader, Recursion & Data Interpretation</p>");
-  
-      // Add the CSS class
-      $(".cont").addClass("transi");
-  
-      // Remove the CSS class after a certain duration
-      setTimeout(function() {
-        $(".cont").removeClass("transi");
-      }, 1000);
-  
-      // Call the change_color function
-      change_color(11);
-    });
-  
-    $("#3").click(function() {
-      $(".cont").html("<h4>Algorithms</h4><p><b>About:</b>This module covers fundamentals of DSA. You'll gain problem-solving skills through practical exercises</p><p><b>Topics Covered:</b>ArrayList, Switch cases, Strings, Sorting, StringBuilder, BufferReader, Recursion & Data Interpretation</p>");
-  
-      // Add the CSS class
-      $(".cont").addClass("transi");
-  
-      // Remove the CSS class after a certain duration
-      setTimeout(function() {
-        $(".cont").removeClass("transi");
-      }, 1000);
-  
-      // Call the change_color function
-      change_color(13);
-    });
-  
-    $("#4").click(function() {
-      $(".cont").html("<h4>Data Structures and Algorithms</h4><p><b>About:</b>This module covers fundamentals of DSA. You'll gain problem-solving skills through practical exercises</p><p><b>Topics Covered:</b>ArrayList, Switch cases, Strings, Sorting, StringBuilder, BufferReader, Recursion & Data Interpretation</p>");
-  
-      // Add the CSS class
-      $(".cont").addClass("transi");
-  
-      // Remove the CSS class after a certain duration
-      setTimeout(function() {
-        $(".cont").removeClass("transi");
-      }, 1000);
-  
-      // Call the change_color function
-      change_color(14);
-    });
+  $("#2").click(function() {
+    $(".cont").html("<h4>Elementary Data Structures and Algorithms</h4><p><b>About:</b>This module covers fundamentals of DSA. You'll gain problem-solving skills through practical exercises</p><p><b>Topics Covered:</b>ArrayList, Switch cases, Strings, Sorting, StringBuilder, BufferReader, Recursion & Data Interpretation</p>");
+
+    // Add the CSS class
+    $(".cont").addClass("transi");
+
+    // Remove the CSS class after a certain duration
+    setTimeout(function() {
+      $(".cont").removeClass("transi");
+    }, 1000);
+
+    // Call the change_color function
+    change_color(12);
   });
-  
-  function change_color(id) {
-    $(".circle").removeClass("circle-blue");
-      var i=id;
-      $("#" + i).addClass("circle-blue");
-        }
-  
+
+  $("#1").click(function() {
+    $(".cont").html("<h4>Data Structures and Algorithms</h4><p><b>About:</b>This module covers fundamentals of DSA. You'll gain problem-solving skills through practical exercises</p><p><b>Topics Covered:</b>ArrayList, Switch cases, Strings, Sorting, StringBuilder, BufferReader, Recursion & Data Interpretation</p>");
+
+    // Add the CSS class
+    $(".cont").addClass("transi");
+
+    // Remove the CSS class after a certain duration
+    setTimeout(function() {
+      $(".cont").removeClass("transi");
+    }, 1000);
+
+    // Call the change_color function
+    change_color(11);
+  });
+
+  $("#3").click(function() {
+    $(".cont").html("<h4>Algorithms</h4><p><b>About:</b>This module covers fundamentals of DSA. You'll gain problem-solving skills through practical exercises</p><p><b>Topics Covered:</b>ArrayList, Switch cases, Strings, Sorting, StringBuilder, BufferReader, Recursion & Data Interpretation</p>");
+
+    // Add the CSS class
+    $(".cont").addClass("transi");
+
+    // Remove the CSS class after a certain duration
+    setTimeout(function() {
+      $(".cont").removeClass("transi");
+    }, 1000);
+
+    // Call the change_color function
+    change_color(13);
+  });
+
+  $("#4").click(function() {
+    $(".cont").html("<h4>Data Structures and Algorithms</h4><p><b>About:</b>This module covers fundamentals of DSA. You'll gain problem-solving skills through practical exercises</p><p><b>Topics Covered:</b>ArrayList, Switch cases, Strings, Sorting, StringBuilder, BufferReader, Recursion & Data Interpretation</p>");
+
+    // Add the CSS class
+    $(".cont").addClass("transi");
+
+    // Remove the CSS class after a certain duration
+    setTimeout(function() {
+      $(".cont").removeClass("transi");
+    }, 1000);
+
+    // Call the change_color function
+    change_color(14);
+  });
+});
+
+function change_color(id) {
+  $(".circle").removeClass("concentric");
+    var i=id;
+    $("#" + i).addClass("concentric");
+      }
